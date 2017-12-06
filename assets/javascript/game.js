bc10e6db18f6c8c5a20610dd9d15aef5279aa663
Nathan Jahnke <naterased@gmail.com>
2017-12-05 22:25:29 -0600

    Almost complete, just need to add winning and losing conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,54 @@
 var wins = 0;
 var losses = 0;
-var wordBank = ["Scranton", "Dwight", "Michael", "Jim"];
+var wordBank = ["scranton", "dwight", "michael", "jim", "creed"];
 var guessesLeft = 15;
-var guessLetter;
+var guessedLetter;
+var gussedLetterArray = new Array();
 var winningWord;
+var displayArray;  // Array for currentWord display
+
 
 // Function that chooses random letter from array named letter defined above.
-function randWord() {
-    var letter = wordBank[Math.floor(Math.random() * wordBank.length)];
-    return letter;
+function randomizeWord() {
+    winningWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    displayArray = new Array(winningWord.length); // Array with 6 slots 
+
+    // Fill displayArray with "_" values.
+    displayArray.fill("_");
+
+    // Display array contents to page.
+    displayCurrentWord();
 }
 
+// Function to display current correct letter guesses on page
+function displayCurrentWord() {
+    document.getElementById("currentWord").innerHTML = " ";  // Clears div on page
 
-$(document).ready(function() {
-    winningWord = randWord();
+    for (i = 0; i < displayArray.length; i++) {
+        document.getElementById("currentWord").innerHTML += displayArray[i] + " ";
+    }
+}
+
+$(document).ready(function () {
+    randomizeWord();
 });
 
 
-document.onkeyup = function(event) {
-
+document.onkeyup = function (event) {
+    // User Input
     guessLetter = String.fromCharCode(event.keyCode).toLowerCase();
-    document.getElementById("guess").innerHTML += guessLetter + ", ";
+    document.getElementById("guess").innerHTML += guessLetter + " ";
+
+    // Check if letter exists in winningWord. Add letter to displayArray at index found.
+    for (i = 0; i < winningWord.length; i++) {
+        if (winningWord.charAt(i) == guessLetter) {
+            displayArray[i] = guessLetter;  // displayArray = ["n", "_", "_", "_", "_", "n"]
+        }
+    }
+    displayCurrentWord();
+
     guessesLeft -= 1;
     document.getElementById("remainingGuess").innerHTML = guessesLeft;
 
-    if (guessLetter.includes(winningWord)) {
-        document.getElementById("currentWord").innerHTML = guessLetter;
-    }
+    var match = guessLetter.match(winningWord);
 }
